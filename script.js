96f84161e4589653a02bab802d17297d33ffbbee
MayC-137 <mayraemaya@gmail.com>
2024-08-08 01:49:26 -0600

    Final
    Versión final del proyecto

---
+++ script.js
@@ -0,0 +1,28 @@
+//movimiento inicio
+document.addEventListener('DOMContentLoaded', function () {
+    var app = document.getElementById('app');
+    var typewriter = new Typewriter(app, {
+        loop: true,
+        cursor: '|',
+    });
+    typewriter.typeString('Programadora')
+        .pauseFor(1500)
+        .deleteAll()
+        .typeString('Desarrolladora Web')
+        .pauseFor(1500)
+        .deleteAll()
+        .typeString('Vendedora')
+        .pauseFor(1500)
+        .deleteAll()
+        .typeString('Diseñadora gráfica') 
+        .pauseFor(1500)
+        .deleteAll()
+        .start();
+});
+
+//movimiento carousel
+    var myCarousel = document.querySelector('#carouselExample');
+    var carousel = new bootstrap.Carousel(myCarousel, {
+        interval: 500, 
+        ride: 'carousel'
+    });
